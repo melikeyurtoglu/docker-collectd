ca3b4c0eb1214e610ee85a47b0137bb432e32807
Yaron Rosenbaum <yaron.rosenbaum@gmail.com>
2014-12-18 19:02:45 +0200

    Upgraded to 5.4.1, building it..

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,9 @@ sleep 20
 echo "That was a good nap. Now to work..."
 
 
-grep -r -l {{LOGSTASH_SERVER}}  /etc/collectd/collectd.d/*.conf | xargs sed -i "s/{{LOGSTASH_SERVER}}/$LOGSTASH_SERVER/g" 
-grep -r -l {{LOGSTASH_PORT}}    /etc/collectd/collectd.d/*.conf | xargs sed -i "s/{{LOGSTASH_PORT}}/$LOGSTASH_PORT/g"
-sed -i "s/{{HOSTNAME}}/$HOSTNAME/g" /etc/collectd/collectd.conf
+grep -r -l {{LOGSTASH_SERVER}}  $basedir/etc/collectd.d/*.conf | xargs sed -i "s/{{LOGSTASH_SERVER}}/$LOGSTASH_SERVER/g" 
+grep -r -l {{LOGSTASH_PORT}}    $basedir/etc/collectd.d/*.conf | xargs sed -i "s/{{LOGSTASH_PORT}}/$LOGSTASH_PORT/g"
+sed -i "s/{{HOSTNAME}}/$HOSTNAME/g" $basedir/etc/collectd.conf
+sed -i "s:{{basedir}}:$basedir:g" $basedir/etc/collectd.conf
 
-exec collectd -f
+exec $basedir/sbin/collectd -f
