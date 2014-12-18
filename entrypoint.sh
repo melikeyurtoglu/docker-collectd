b2afc97dc497c8167a76036d7e9f573f79c14802
Yaron Rosenbaum <yaron.rosenbaum@gmail.com>
2014-12-18 13:24:38 +0200

    added exit on error setting on script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+set -eo pipefail
 
 echo "Taking a nap, to allow weave network to properly set up.."
 sleep 20
 echo "That was a good nap. Now to work..."
+
 
 grep -r -l {{LOGSTASH_SERVER}}  /etc/collectd/collectd.d/*.conf | xargs sed -i "s/{{LOGSTASH_SERVER}}/$LOGSTASH_SERVER/g" 
 grep -r -l {{LOGSTASH_PORT}}    /etc/collectd/collectd.d/*.conf | xargs sed -i "s/{{LOGSTASH_PORT}}/$LOGSTASH_PORT/g"
